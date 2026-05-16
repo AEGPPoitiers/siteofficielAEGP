266209b58a8db0f0f45ae536baeecd784a41d106
haroldjior <ilan.guerizec@proton.me>
2026-05-16 21:05:46 +0200

    feat: init frontend for Vercel account creation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Agenda from './pages/Agenda'
+import Tutorat from './pages/Tutorat'
+import Login from './pages/Login'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/tutorat" element={<Tutorat />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </Routes>
+  )
+}
