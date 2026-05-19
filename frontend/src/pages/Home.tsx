9d64941676b659f7a0d9e0ef25bd2127eccb6210
haroldjior <ilan.guerizec@proton.me>
2026-05-20 00:08:57 +0200

    fix: couleur du hover (#11)
    Changé la couleur de hover:indigo a hover:black sur le link vers boite a idee sur la page Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
         <Link
           to="/boiteaidee"
-          className="block p-6 bg-white rounded-lg border border-gray-200 hover:border-indigo-400 hover:shadow-md transition-all"
+          className="block p-6 bg-white rounded-lg border border-gray-200 hover:border-black hover:shadow-md transition-all"
         >
           <h2 className="text-2xl font-semibold text-gray-900 mb-2">
             Boîte à idées
